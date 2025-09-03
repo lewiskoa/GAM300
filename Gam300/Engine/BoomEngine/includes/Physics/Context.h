7c30ae43df7f37a4900e4089c28a57adab9be0ba
TheBrosefStalin <tgywesley@gmail.com>
2025-09-03 19:42:23 +0800

    8.11

--- Gam300/Engine/BoomEngine/includes/Physics/Context.h
+++ Gam300/Engine/BoomEngine/includes/Physics/Context.h
@@ -3,13 +3,13 @@
 #include "Utilities.h"
 
 namespace Boom {
-	struct PhysicsContext {
-		BOOM_INLINE PhysicsContext() {
+    struct PhysicsContext {
+        BOOM_INLINE PhysicsContext() {
             // create worker threads
             m_Dispatcher = PxDefaultCpuDispatcherCreate(2);
 
             // create a scene desciption
-            PxSceneDesc sceneDesc(m_Physics -> getTolerancesScale());
+            PxSceneDesc sceneDesc(m_Physics->getTolerancesScale());
             sceneDesc.simulationEventCallback = &m_EventCallback;
             sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
             sceneDesc.filterShader = CustomFilterShader;
@@ -35,6 +35,156 @@ namespace Boom {
             if (m_Foundation) { m_Foundation->release(); }
         }
 
+        // Rigid Body
+        BOOM_INLINE void AddRigidBody(Entity& entity) {
+            auto& transform = entity.template
+                Get<TransformComponent>().Transform;
+            auto& body = entity.template
+                Get<RigidBodyComponent>().RigidBody;
+            bool hasCollider = entity.template
+                Has<ColliderComponent>();
+
+
+
+            // create rigidbody transformation
+            PxTransform pose(ToPxVec3(transform.translate));
+            glm::quat rot(transform.rotate);
+            pose.q = PxQuat(rot.x, rot.y, rot.z, rot.w);
+
+            // create a rigid body actor
+            if (entity.template Has<ColliderComponent>())
+            {
+                // create collider shape
+
+                auto& collider = entity.template
+                    Get<ColliderComponent>().Collider;
+
+                // create collider material
+                collider.Material = m_Physics -> createMaterial(collider.StaticFriction,
+                collider.DynamicFriction,
+                collider.Restitution);
+
+                if (collider.Type == Collider3D::BOX)
+                {
+                    PxBoxGeometry
+                        box(ToPxVec3(transform.scale / 2.0f));
+                    collider.Shape = m_Physics -> createShape(box, *collider.Material);
+                }
+                else if (collider.Type == Collider3D::SPHERE)
+                {
+                    PxSphereGeometry
+                        sphere(transform.scale.x / 2.0f);
+                    collider.Shape = m_Physics -> createShape(sphere, *collider.Material);
+                }
+                else if (collider.Type == Collider3D::MESH)
+
+
+                {
+                    // coming next!
+                }
+                else
+                {
+                    BOOM_ERROR("Error creating collider invalid type provided");
+                        return;
+                }
+
+                // create actor instanace
+
+                if (body.Type == RigidBody3D::DYNAMIC)
+                {
+                    body.Actor = PxCreateDynamic(*m_Physics,
+                        pose, *collider.Shape, body.Density);
+                    body.Actor -> setActorFlag(PxActorFlag::eSEND_SLEEP_NOTIFIES, true);
+                }
+                else if (body.Type == RigidBody3D::STATIC)
+                {
+                    body.Actor = PxCreateStatic(*m_Physics,
+                        pose, *collider.Shape);
+                }
+            }
+            else
+            {
+                if (body.Type == RigidBody3D::DYNAMIC)
+                {
+                    body.Actor = m_Physics -> createRigidDynamic(pose);
+                }
+                else if (body.Type == RigidBody3D::STATIC)
+                {
+                    body.Actor = m_Physics -> createRigidStatic(pose);
+
+
+                }
+            }
+
+            // check actor
+            if (!body.Actor)
+            {
+                BOOM_ERROR("Error creating dynamic actor");
+                return;
+            }
+
+            // set user data to entt id
+            body.Actor->userData = new EntityID(entity.ID());
+
+            // add actor to the m_Scene
+            m_Scene->addActor(*body.Actor);
+        }
+    
+        
+             
+        // Mesh Colliders
+        BOOM_INLINE PxConvexMeshGeometry CookMesh(const MeshData<ShadedVert>& data) {
+            // px vertex container
+            std::vector<PxVec3> vertices;
+
+
+
+            // convert position attributes
+            for (auto& vertex : data.vtx)
+            {
+                vertices.push_back(ToPxVec3(vertex.pos));
+            }
+
+            PxConvexMeshDesc meshDesc;
+            // vertices
+            meshDesc.points.data = vertices.data();
+            meshDesc.points.stride = sizeof(PxVec3);
+            meshDesc.points.count = vertices.size();
+            // indices
+            meshDesc.indices.data = data.idx.data();
+            meshDesc.indices.count = data.idx.size();
+            // flags
+            meshDesc.flags = PxConvexFlag::eCOMPUTE_CONVEX;
+
+            // cooking the mesh
+            PxCookingParams cookingParams =
+                PxCookingParams(PxTolerancesScale());
+            PxCooking* cooking = PxCreateCooking(PX_PHYSICS_VERSION,
+                *m_Foundation, cookingParams);
+            PxConvexMeshCookingResult::Enum result;
+            PxConvexMesh* convexMesh = cooking -> createConvexMesh(meshDesc,
+    m_Physics->getPhysicsInsertionCallback(), &result);
+            PxConvexMeshGeometry convexMeshGeometry(convexMesh);
+
+            cooking->release();
+            return convexMeshGeometry;
+        
+        }
+
+
+        BOOM_INLINE void Simulate(uint32_t step, float dt)
+        {
+            for (int i = 0; i < step; ++i)
+            {
+                // simulate m_Physics for a time step
+                m_Scene->simulate(dt);
+
+                // block until simulation is complete
+
+
+                m_Scene->fetchResults(true);
+            }
+        }
 
 
     private:
