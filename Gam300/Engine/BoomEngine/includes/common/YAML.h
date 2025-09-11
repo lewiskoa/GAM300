af10b43a846b52865ed09155d96381268718baae
ahodigipen <a.ho@digipen.edu>
2025-09-11 13:05:24 +0800

    Added YAML library and helper functions to convert custom types

---
+++ Gam300/Engine/BoomEngine/includes/common/YAML.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "Core.h"
+#include "yaml-cpp/yaml.h"
+
+namespace YAML
+{
+	template<>
+	struct convert<glm::vec3>
+	{
+		BOOM_INLINE static Node encode(const glm::vec3& rhs)
+		{
+			Node node;
+			node.push_back(rhs.x);
+			node.push_back(rhs.y);
+			node.push_back(rhs.z);
+			node.SetStyle(EmitterStyle::Flow);
+			return node;
+		}
+		BOOM_INLINE static bool decode(const Node& node, glm::vec3& rhs)
+		{
+			if (!node.IsSequence() || node.size() != 3)
+				return false;
+			rhs.x = node[0].as<float>();
+			rhs.y = node[1].as<float>();
+			rhs.z = node[2].as<float>();
+			return true;
+		}
+	};
+
+	//stream operator
+	BOOM_INLINE Emitter& operator<<(Emitter& out, const glm::vec3& v)
+	{
+		out << Flow;
+		out << BeginSeq << v.x << v.y << v.z << EndSeq;
+		return out;
+	}
+}
