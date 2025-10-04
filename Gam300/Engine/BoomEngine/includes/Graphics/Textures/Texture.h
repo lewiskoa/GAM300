1124910cac498f4a0a8a7c981ab574357d0966ad
Darius <d.chan@digipen.edu>
2025-10-04 21:35:10 +0800

    added compression format code

--- Gam300/Engine/BoomEngine/includes/Graphics/Textures/Texture.h
+++ Gam300/Engine/BoomEngine/includes/Graphics/Textures/Texture.h
@@ -1,6 +1,7 @@
 #pragma once
 namespace Boom {
 	struct BOOM_API Texture2D {
+
 		Texture2D() = default;
 		//file path starts from Textures folder
 		Texture2D(std::string filename, bool isFlipY = true, bool isHDR = false);
@@ -9,10 +10,16 @@ namespace Boom {
 		void Bind();
 		void UnBind();
 
+		void LoadUnCompressed(std::string const& filename, bool isFlipY, bool isHDR);
+		void LoadCompressed(std::string const& filename, bool isFlipY);
+
 	public: //member manipulators
 		operator uint32_t() const noexcept;
 		int32_t Height() const noexcept;
 		int32_t Width() const noexcept;
+
+	protected: //helpers
+		std::string GetExtension(std::string const& filename);
 
 	private:
 		int32_t height;
--- Gam300/Engine/BoomEngine/src/Textures/Texture.cpp
+++ Gam300/Engine/BoomEngine/src/Textures/Texture.cpp
@@ -8,17 +8,33 @@
 #include <stb_image.h>
 #pragma warning(pop)
 
+#include <gli/gli.hpp>
+
 namespace Boom {
 	Texture2D::Texture2D(std::string filename, bool isFlipY, bool isHDR)
 		: height{}, width{}, id{}
 	{
 		filename = CONSTANTS::TEXTURES_LOCATION.data() + filename;
 
+		std::string ext{ GetExtension(filename) };
+		if (ext == "dds") {
+			LoadCompressed(filename, isFlipY);
+		}
+		else {
+			LoadUnCompressed(filename, isFlipY, isHDR);
+		}
+	}
+	Texture2D::~Texture2D() {
+		glDeleteTextures(1, &id);
+	}
+
+	void Texture2D::LoadUnCompressed(std::string const& filename, bool isFlipY, bool isHDR) {
 		//flip y axis (needed operation for many image types)
 		stbi_set_flip_vertically_on_load(isFlipY);
 
 		//texture data
 		void* pixels{};
+
 		if (isHDR) {
 			int32_t channels{};
 			pixels = stbi_loadf(filename.c_str(), &width, &height, &channels, 0);
@@ -51,8 +67,61 @@ namespace Boom {
 
 		glBindTexture(GL_TEXTURE_2D, 0);
 	}
-	Texture2D::~Texture2D() {
-		glDeleteTextures(1, &id);
+	void Texture2D::LoadCompressed(std::string const& filename, bool isFlipY) {
+		// Load DDS using GLI
+		gli::texture texture = gli::load(filename);
+		if (texture.empty()) {
+			BOOM_ERROR("failed Texture2D::Load({}) - Could not load DDS file", filename);
+			return;
+		}
+
+		// Ensure it's a 2D texture and DXT1 format
+		if (texture.target() != gli::TARGET_2D) {
+			BOOM_ERROR("failed Texture2D::Load({}) - Only 2D DDS textures supported", filename);
+			return;
+		}
+		gli::gl gl(gli::gl::PROFILE_GL33); // Adjust for your OpenGL version
+		gli::gl::format format = gl.translate(texture.format(), texture.swizzles());
+		if (format.Internal != gli::gl::INTERNAL_RGBA_DXT1) {
+			BOOM_ERROR("failed Texture2D::Load({}) - DDS must be DXT1 format", filename);
+			return;
+		}
+
+		glGenTextures(1, &id);
+		glBindTexture(GL_TEXTURE_2D, id);
+
+		gli::texture2d tex2D(texture);
+		if (isFlipY) {
+			tex2D = gli::flip(tex2D);
+		}
+		width = (int32_t)tex2D.extent().x;
+		height = (int32_t)tex2D.extent().y;
+		GLsizei compressed_size = (GLsizei)tex2D.size(0);
+		const void* compressed_data = tex2D.data(0, 0, 0);
+
+		glCompressedTexImage2D(
+			GL_TEXTURE_2D,
+			0,
+			GL_COMPRESSED_RGBA_S3TC_DXT1_EXT,
+			width,
+			height,
+			0,
+			compressed_size,
+			compressed_data
+		);
+
+		if (glGetError() != GL_NO_ERROR) {
+			BOOM_ERROR("failed Texture2D::Load({}) - OpenGL error during DDS upload", filename);
+			glDeleteTextures(1, &id);
+			return;
+		}
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+		glBindTexture(GL_TEXTURE_2D, 0);
 	}
 
 	//set's texture's active unit and uniform to graphics
@@ -72,4 +141,14 @@ namespace Boom {
 	Texture2D::operator uint32_t() const noexcept { return id; }
 	int32_t Texture2D::Height() const noexcept { return height; }
 	int32_t Texture2D::Width() const noexcept { return width; }
+
+	std::string Texture2D::GetExtension(std::string const& filename) {
+		uint32_t pos{ (uint32_t)filename.find_last_of('.') };
+		if (pos == std::string::npos || pos == filename.length() - 1) {
+			return ""; //no extension
+		}
+		std::string ext{ filename.substr(pos + 1) };
+		std::transform(ext.begin(), ext.end(), ext.begin(), ::tolower); //lowercase
+		return ext;
+	}
 }
